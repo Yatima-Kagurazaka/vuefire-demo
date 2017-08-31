86db2ab5fd7a2b494d9f79fd2c30cc9d0af9b4a2
Yatima Kagurazaka <Yatima.Kgr@gmail.com>
2017-08-31 10:04:29 +0900

    personalize

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,10 @@
 import Firebase from 'firebase'
 
 export default Firebase.initializeApp({
-  apiKey: "AIzaSyAH2VXLGPWY-XsZokwcIVWODGk_zrrovTI",
-  authDomain: "vuefire-demo.firebaseapp.com",
-  databaseURL: "https://vuefire-demo.firebaseio.com",
-  storageBucket: "vuefire-demo.appspot.com",
-  messagingSenderId: "925973227441"
+    apiKey: "AIzaSyCFKI-s3Pzte2HNjTxN6DFF2WAjHnh7OCo",
+    authDomain: "vuefire-demo-d4f54.firebaseapp.com",
+    databaseURL: "https://vuefire-demo-d4f54.firebaseio.com",
+    projectId: "vuefire-demo-d4f54",
+    storageBucket: "vuefire-demo-d4f54.appspot.com",
+    messagingSenderId: "103426811601"
 })
